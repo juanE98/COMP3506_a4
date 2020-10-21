e7e2ba7ee36b4ef5ebe14c4a6293874c56055ea0
JuanEspares <juloespares@yahoo.com.au>
2020-10-21 22:18:34 +1000

    Has duplicates for addTrace

--- src/ContactTracer.java
+++ src/ContactTracer.java
@@ -1,9 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
 public class ContactTracer {
-    HashMap<String, HashMap<String, Integer>> adjMap;
+
+    //Adjacency map representation of graph.
+    HashMap<String, HashMap<String, ArrayList<Integer>>> adjMap;
 
     /**
      * Initialises an empty ContactTracer with no populated contact traces.
@@ -22,19 +26,7 @@ public class ContactTracer {
     public ContactTracer(List<Trace> traces) {
         this.adjMap = new HashMap<>();
         for (Trace trace : traces) {
-            String vertex = trace.getPerson1();
-            String neighbour = trace.getPerson2();
-            int time = trace.getTime();
-            HashMap<String, Integer> neighbours;
-            if (adjMap.containsKey(trace.getPerson1())) {
-                adjMap.get(vertex).put(neighbour, time);
-            }
-            else {
-                neighbours = new HashMap<>();
-                neighbours.put(neighbour,time);
-                adjMap.put(vertex,neighbours);
-            }
-
+            addTrace(trace);
         }
     }
 
@@ -51,22 +43,94 @@ public class ContactTracer {
         String person1 = trace.getPerson1();
         String person2 = trace.getPerson2();
         int time = trace.getTime();
-        HashMap<String,Integer> compareNode = new HashMap<>();
-        compareNode.put(person2,time);
-        HashMap<String,Integer> compareOtherNode = new HashMap<>();
-        compareOtherNode.put(person1,time);
 
-        if (adjMap.containsKey(person1) && adjMap.containsValue(compareNode)) {
-            return;
+        if (adjMap.containsKey(person1) && adjMap.containsKey(person2)
+                && !checkExisitingTrace(adjMap,trace)) {
+            HashMap<String, ArrayList<Integer>> neighbour = new HashMap<>();
+            ArrayList<Integer> times = new ArrayList<>();
+            times.add(time);
+            if (adjMap.get(person1).get(person2) == null) {
+                neighbour.put(person2,times);
+                adjMap.put(person1, neighbour);
+                adjMap.put(person2,neighbour);
+            }
+            else if (adjMap.get(person2).get(person1) == null) {
+                neighbour.put(person1,times);
+                adjMap.put(person2, neighbour);
+                adjMap.put(person1,neighbour);
+            }
+            else {
+                adjMap.get(person1).get(person2).add(time);
+                adjMap.get(person2).get(person1).add(time);
+            }
         }
-        else if (adjMap.containsKey(person2) && adjMap.containsValue(compareOtherNode)){
-            return;
+
+        else if (adjMap.containsKey(person1) && !adjMap.containsKey(person2)) {
+            ArrayList<Integer> times = new ArrayList<Integer>();
+            times.add(time);
+            HashMap<String, ArrayList<Integer>> neighbour1 =
+                    new HashMap<>();
+            neighbour1.put(person1,times);
+            adjMap.put(person2,neighbour1);
+            adjMap.get(person1).put(person2,times);
+
         }
+        else if (adjMap.containsKey(person2) && !adjMap.containsKey(person1)) {
+            ArrayList<Integer> times = new ArrayList<Integer>();
+            times.add(time);
+            HashMap<String, ArrayList<Integer>> neighbour1 =
+                    new HashMap<>();
+            neighbour1.put(person2,times);
+            adjMap.put(person1,neighbour1);
+            adjMap.get(person2).put(person1,times);
+        }
+
         else {
-            HashMap<String, Integer> newNeighbour = new HashMap<>();
-            newNeighbour.put(person2,time);
-            adjMap.put(person1,newNeighbour);
+            ArrayList<Integer> times = new ArrayList<Integer>();
+            times.add(time);
+            HashMap<String, ArrayList<Integer>> neighbour1 =
+                    new HashMap<>();
+            neighbour1.put(person2,times);
+            HashMap<String, ArrayList<Integer>> neighbour2 =
+                    new HashMap<>();
+            neighbour2.put(person1,times);
+            adjMap.put(person1,neighbour1);
+            adjMap.put(person2,neighbour2);
         }
+    }
+
+    private static void addOther (HashMap<String, HashMap<String,
+            ArrayList<Integer>>> adjMap,int time, String person1,
+                                  String person2) {
+        ArrayList<Integer> times = new ArrayList<Integer>();
+        times.add(time);
+        HashMap<String, ArrayList<Integer>> neighbour1 =
+                new HashMap<>();
+        neighbour1.put(person1,times);
+        adjMap.put(person2,neighbour1);
+        adjMap.get(person1).put(person2,times);
+    }
+
+    /**
+     * Helper method to check if a contact trace involving the same two people
+     * at the exact same time is already stored.
+     * @param adjMap
+     * @param trace
+     * @return true if time involving two people is stored, false otherwise.
+     */
+    private static boolean checkExisitingTrace (HashMap<String, HashMap<String,
+            ArrayList<Integer>>> adjMap, Trace trace) {
+        String person1 = trace.getPerson1();
+        String person2 = trace.getPerson2();
+        int timeCheck = trace.getTime();
+        if (adjMap.get(person1).get(person2) != null
+                && adjMap.get(person2).get(person1) != null) {
+            if (adjMap.get(person1).get(person2).contains(timeCheck)
+                    || adjMap.get(person2).get(person1).contains(timeCheck) ) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
